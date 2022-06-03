a36c9f1a301f2ec1769fe14aa517e858d09550d4
Renato Barros <renato.barros@elo.com.br>
2022-06-03 20:29:09 -0300

    Initial commit of creation folders and files

---
+++ main.js
@@ -0,0 +1,35 @@
+const tocaSomPom = () => {
+    document.querySelector('#som_tecla_pom').play();
+};
+
+const tocaSomClap= () => {
+    document.querySelector('#som_tecla_clap').play();
+};
+
+const tocaSomTim= () => {
+    document.querySelector('#som_tecla_tim').play();
+};
+
+const tocaSomPuff= () => {
+    document.querySelector('#som_tecla_puff').play();
+};
+
+const tocaSomSplash= () => {
+    document.querySelector('#som_tecla_splash').play();
+};
+
+const tocaSomToim= () => {
+    document.querySelector('#som_tecla_toim').play();
+};
+
+const tocaSomPsh= () => {
+    document.querySelector('#som_tecla_psh').play();
+};
+
+const tocaSomTic= () => {
+    document.querySelector('#som_tecla_tic').play();
+};
+
+const tocaSomTom= () => {
+    document.querySelector('#som_tecla_tom').play();
+};
